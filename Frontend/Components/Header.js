4850012cc5a1e0427cc1ff8c5d802fa01f157442
Enmanuel Urena <enmanuelurenav@gmail.com>
2022-09-14 12:19:32 -0400

    First commit

---
+++ Frontend/Components/Header.js
@@ -0,0 +1,8 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export const Header = () => {
+  return(
+    <Text>Header test</Text>
+  )
+}
