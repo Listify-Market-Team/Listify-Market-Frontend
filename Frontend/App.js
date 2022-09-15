8c31b9bca6be4757ac46e302c09633b53fa01e8d
enmanuelurena <enmanuelurenav@gmail.com>
2022-09-15 00:27:44 -0400

    Add header components, styles and fonts

--- Frontend/App.js
+++ Frontend/App.js
@@ -2,21 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Header } from './Components/Header';
 
+const viewName = 'Home'
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Header/>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header title={viewName}/>
+      <StatusBar style="auto"/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
--- Frontend/Components/Header.js
+++ Frontend/Components/Header.js
@@ -1,8 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
+import { SimpleLineIcons } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import { useFonts } from "expo-font";
 
-export const Header = () => {
-  return(
-    <Text>Header test</Text>
-  )
-}
+const MenuIcon = () => {
+  return (
+    <TouchableHighlight onPress={() => {}} style={styles.MenuIcon}>
+      <View>
+        <SimpleLineIcons name="menu" size={24} color="black" />
+      </View>
+    </TouchableHighlight>
+  );
+};
+
+const HelpIcon = () => {
+  return (
+    <TouchableHighlight onPress={() => {}}>
+      <View>
+        <Feather name="help-circle" size={24} color="black" />
+      </View>
+    </TouchableHighlight>
+  );
+};
+
+const HeaderTitle = ({ title }) => {
+  return <Text style={styles.HeaderTitle}>{title}</Text>;
+};
+
+export const Header = ({ title }) => {
+  const [fontsLoaded] = useFonts({
+    "Inter-SemiBold": require("../assets/fonts/Inter-SemiBold.ttf"),
+  });
+  return (
+    <View style={styles.container}>
+      <MenuIcon />
+      <HeaderTitle title={title} />
+      <HelpIcon />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 14,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: 'center',
+    backgroundColor: "#fff",
+  },
+  MenuIcon: {
+    // Algun codigo de estilos va aqui, cual no se
+  },
+  HeaderTitle: {
+    fontFamily: "Inter-SemiBold",
+    fontSize: 16,
+  },
+});
