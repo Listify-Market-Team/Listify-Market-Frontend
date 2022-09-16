4e9c80e4e2c4f5fa9c91d3e40d8981f367af13d0
Luimi55 <63078542+Luimi55@users.noreply.github.com>
2022-09-15 23:04:17 -0400

    Add Search Bar component

---
+++ Frontend/Components/SearchBar.js
@@ -0,0 +1,58 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  Text,
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Image
+} from "react-native";
+import searchIcon from "../resources/1024px-Search_Icon.svg.png"
+
+const SearchBar = () => {
+  return (
+    <View style={styles.container}>
+      <TextInput style={styles.textInput}  />
+
+      <TouchableOpacity
+          style={styles.searchButton}
+          activeOpacity={0.5}>
+
+          <Image
+            source={searchIcon}
+            style={styles.buttonImageIconStyle}
+          />
+    </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    borderRadius: "15px",
+    backgroundColor: "#d9e8e9",
+    width: "fit-content"
+  },
+  textInput: {
+    width: "100%",
+    borderRadius: "15px",
+    padding: "4px",
+    backgroundColor: "#d9e8e9",
+  },
+  searchButton: {
+    alignItems: "center",
+    backgroundColor: "#00d36e",
+    borderWidth: 0.5,
+    borderColor: "#fff",
+    borderRadius: "15px",
+  },
+  buttonImageIconStyle: {
+    margin: 3,
+    height: 19,
+    width: 19,
+    resizeMode: 'stretch',
+  },
+});
+
+export default SearchBar;
