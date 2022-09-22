ceb78e3d8ada8779b1900f34652fffd5d3c93e68
Alberto <dominguez_alberto@mediatrix.com.do>
2022-09-22 17:09:08 -0400

    Add ProductsMaintance component

---
+++ Frontend/Components/ProductsMaintance/ProductsMaintance.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const ProductsMaintance = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#024059" }}>
+    <Text>
+        <b>Mantenimiento de productos funciona</b>
+      </Text>
+    </View>    
+  );
+}
+
+export default ProductsMaintance;
