5bdc16308ce2dcbcbdb72efc75f395a75b0f14b7
Alberto <dominguez_alberto@mediatrix.com.do>
2022-09-22 17:17:24 -0400

    Add Strech to ProductMaintance

--- Frontend/Components/ProductsMaintance/ProductsMaintance.js
+++ Frontend/Components/ProductsMaintance/ProductsMaintance.js
@@ -3,8 +3,8 @@ import { Text, View } from 'react-native';
 
 const ProductsMaintance = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#024059" }}>
-    <Text>
+    <View style={{ flex: 1, alignSelf: "stretch", justifyContent: "center", alignItems: "center", backgroundColor: "#024059" }}>
+    <Text style={{ color: "white" }}>
         <b>Mantenimiento de productos funciona</b>
       </Text>
     </View>    
