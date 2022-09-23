5e2f75636988d7afb26df56f4e6e3e3103e875be
Enmanuel Urena <enmanuelurenav@gmail.com>
2022-09-23 15:49:06 -0400

    Set stack header styles

--- Frontend/Navigation/StackNavigator.js
+++ Frontend/Navigation/StackNavigator.js
@@ -1,22 +1,36 @@
 import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View } from "react-native";
+import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from "../Screens/HomeScreen";
 import { MenuIcon, HelpIcon, BackIcon } from "../Components/Header";
 import HelpScreen from "../Screens/HelpScreen"
 
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 export const MainStackNavigator = () => {
   return (
     <Stack.Navigator  screenOptions={{
-      headerLeft: () => <MenuIcon />,
-      headerRight: () => <HelpIcon />,
+      headerLeft: () => (
+        <View style={{flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center', left:10, paddingRight: 20}}>
+          <MenuIcon/>
+        </View>
+      ),
+      headerRight: () => (
+        <View style={{flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center', right:10}}>
+          <HelpIcon/>
+        </View>
+      ),
     }}>
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Help" component={HelpScreen} options={{
-        headerLeft: () => <BackIcon />,
+        headerLeft: () => (
+        <View style={{flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center', left:10, paddingRight: 20}}>
+          <BackIcon/>
+        </View>
+      ),
         headerRight: () => null,
+        headerShadowVisible: false,
         title: '',
         headerStyle:{
           backgroundColor: '#B5D3D3'
