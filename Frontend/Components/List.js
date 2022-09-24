80b7fe7a8c0666559df6297a5a8a527883901bda
Luimi55 <63078542+Luimi55@users.noreply.github.com>
2022-09-24 09:04:27 -0400

    Create List component

---
+++ Frontend/Components/List.js
@@ -0,0 +1,18 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const List = () => {
+  return (
+    <View>
+      <Text>List</Text>
+    </View>
+  )
+}
+
+const style = StyleSheet.create({
+    lettle:{
+        color:"red"
+    },
+})
+
+export default List
