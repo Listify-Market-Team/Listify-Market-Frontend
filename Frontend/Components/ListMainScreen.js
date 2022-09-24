f0f4c51b5f0873edcf1adecd752cc3bfa80d002e
Luimi55 <63078542+Luimi55@users.noreply.github.com>
2022-09-24 09:01:20 -0400

    Add List main screen component without lists

--- Frontend/Components/ListMainScreen.js
+++ Frontend/Components/ListMainScreen.js
@@ -1,28 +1,78 @@
+import { useEffect, useState } from "react";
 import { StatusBar } from "react-native";
-import {StyleSheet, View, Text, Button } from "react-native";
+import { StyleSheet, View, Text, Button, TouchableOpacity } from "react-native";
+import SearchBar from "./SearchBar";
+import List from "./List";
+
+// const fetchList = async () => {
+//     const res = await fetch("https://localhost:7209/api/Inventory/Getall")
+//     const json = await res.json();
+// console.log(json)
+// }
+// fetchList()
 
 const ListMainScreen = () => {
-return(
+  // useEffect(() => {
+  //     fetchList()
+  // }, [])
+  return (
     <View style={styles.screen}>
-        
+
+    <View style={styles.searchBarContainer}>
+    <SearchBar style={styles.searchBar}/>
     </View>
-);
-}
+
+      <View style={styles.container}>
+        <View style={styles.buttonTituleContainer}>
+          <Text style={styles.titule}>My lists</Text>
+          <TouchableOpacity style={styles.addNewBoton}>
+            <Text>ADD NEW</Text>
+          </TouchableOpacity>
+        </View>
+        <List/>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-screen:{
+  screen: {
     flex: 1, // investigar
-    backgroundColor: "#b5d3d2"
-},
-// content1:{
-//     width: "100%",
-//     height: "15%",
-//     alignItems: "center", // investigar
-//     justifyContent: "center",
-//     backgroundColor: "green"
+    backgroundColor: "#b5d3d2",
+    alignItems: "center",
+  },
+  searchBarContainer:{
+    alignItems: "center",
+    width:"100%",
+    marginVertical: "6%"
+  },
+  container: {
+    paddingTop: "10%",
+    paddingHorizontal: "6%",
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#dae8e9",
+  },
+  buttonTituleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: 'space-between',
+  },
+  titule: {
+    fontFamily: "Verdana",
+    fontSize: "180%",
+    fontWeight: "bold",
+  },
+  addNewBoton: {
+    alignItems: "center",
+    justifyContent: 'center',
+    backgroundColor: "#ffffff",
+    borderWidth: 2,
+    borderColor: "#b5b7b7",
+    padding: "5%",
+    borderRadius: "15px",
+    width: "40%",
+  },
+});
 
-// }
-})
-
-export default ListMainScreen
-
+export default ListMainScreen;
