dea542f1be8a88419755c406eda9d4bab0e08d40
Luimi55 <63078542+Luimi55@users.noreply.github.com>
2022-09-25 09:59:53 -0400

    Add useFetch hook in List component

--- Frontend/Components/List.js
+++ Frontend/Components/List.js
@@ -1,15 +1,39 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet,Image } from 'react-native'
+import React, {useState, useEffect} from 'react'
+import useFetch from '../Hooks/useFetch'
 
 const List = () => {
+// const [list, setList] = useState([])
+
+// const fetchLists = async () =>{
+// const res = await fetch("https://jsonplaceholder.typicode.com/photos");
+// const json =  await res.json();
+// setList(json)
+// console.log(list.title)
+// }
+
+// useEffect(() =>{
+//   fetchLists()
+// },[])
+
+const {loading, data:list} = useFetch("https://jsonplaceholder.typicode.com/photos")
   return (
-    <View>
-      <Text>List</Text>
+    <View style={styles.container}>
+
+      {/* <Image source={list}/> */}
+
+      <Text></Text>
     </View>
   )
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
+    container:{
+      flexDirection: "row",
+      backgroundColor: "#ffffff",
+      padding:10
+
+    },
     lettle:{
         color:"red"
     },
