42266132aac62f642939caf76da1a0a84096a0c6
Alam Sierra <sierra_alam@outlook.com>
2022-09-25 22:49:07 -0400

    create navigation components

---
+++ components/Navigation/DrawerNavigator.js
@@ -0,0 +1,38 @@
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Ionicons } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import Menu from "../../components/Menu";
+import HomeScreen from "../../screens/HomeScreen";
+import HelpScreen from "../../screens/HelpScreen";
+
+const Drawer = createDrawerNavigator();
+
+export const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator
+      drawerContent={props => <Menu {...props} />}
+      initialRouteName="Home"
+    >
+      <Drawer.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: "Home",
+          drawerIcon: () => (
+            <Ionicons name="home-outline" size={24} color="#00DE68" />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Help"
+        component={HelpScreen}
+        options={{
+          title: "Help",
+          drawerIcon: () => (
+            <Feather name="help-circle" size={24} color="#00DE68" />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+};
