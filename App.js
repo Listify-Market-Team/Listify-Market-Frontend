1b0a027ceced36c5b3ed559220dfa1033996c7ad
Derick <derick290801@gmail.com>
2022-10-06 21:02:54 -0400

    searchProductScreen

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
-import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
-import { MainStackNavigator } from "./components/Navigation/StackNavigator";
-import { DrawerNavigator } from "./components/Navigation/DrawerNavigator";
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native'
+import { MainStackNavigator } from './components/Navigation/StackNavigator'
+import { DrawerNavigator } from './components/Navigation/DrawerNavigator'
+import SearchProduct from './screens/SearchProductScreen'
 
 export default function App() {
   return (
-    <NavigationContainer>
-      {/* <MainStackNavigator /> */}
-      <DrawerNavigator />
-    </NavigationContainer>
-  );
+    // <NavigationContainer>
+    //   {/* <MainStackNavigator /> */}
+    //   <DrawerNavigator />
+    // </NavigationContainer>
+    <SearchProduct />
+  )
 }
