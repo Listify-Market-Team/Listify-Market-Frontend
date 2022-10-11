c2d7876a8f395614d341838a030fc2f5c64158d6
Derick <derick290801@gmail.com>
2022-10-10 22:09:14 -0400

    changing name tu searchProductScreen

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import 'react-native-gesture-handler'
-import { NavigationContainer } from '@react-navigation/native'
-import { MainStackNavigator } from './components/Navigation/StackNavigator'
-import { DrawerNavigator } from './components/Navigation/DrawerNavigator'
-import SearchProduct from './screens/SearchProductScreen'
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { MainStackNavigator } from "./components/Navigation/StackNavigator";
+import { DrawerNavigator } from "./components/Navigation/DrawerNavigator";
+import SearchProduct from "./screens/SearchProductScreen";
 
 export default function App() {
   return (
@@ -11,5 +11,5 @@ export default function App() {
     //   <DrawerNavigator />
     // </NavigationContainer>
     <SearchProduct />
-  )
+  );
 }
