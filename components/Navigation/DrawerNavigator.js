989e87248461feeffbcba1baf70827b817fd0252
Alam Sierra <sierra_alam@outlook.com>
2022-12-09 07:25:16 -0400

    move nav stack begin drawer

--- components/Navigation/DrawerNavigator.js
+++ components/Navigation/DrawerNavigator.js
@@ -1,127 +1,77 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { Ionicons } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
-import { AntDesign } from "@expo/vector-icons";
-import { Entypo } from "@expo/vector-icons";
-import { View, StyleSheet } from "react-native";
+import { View } from "react-native";
+import { useTranslation } from "react-i18next";
 import Menu from "../../components/Menu";
 import { MenuIcon, HelpIcon, TranslateIcon } from "../Header";
-import HomeScreen from "../../screens/HomeScreen";
-import HelpScreen from "../../screens/HelpScreen";
-import LoginScreen from "../../screens/LoginScreen";
-import RegisterScreen from "../../screens/RegisterScreen";
-import { MainStackNavigator } from "./StackNavigator";
-import ListMainScreen from "../../screens/ListMainScreen";
-import ProductInfoScreen from "../../screens/ProductInfoScreen";
-import NewListScreen from "../../screens/NewListScreen";
-import UpdateListScreen from "../../screens/UpdateListScreen";
-import AddProductScreen from "../../screens/AddProductScreen";
-import SearchProductScreen from "../../screens/SearchProductScreen";
-import ProductListScreen from "../../screens/productListScreen";
-import { useTranslation } from "react-i18next";
+
+import {
+  InventoriesStackNavigator,
+  ProductsStackNavigator,
+  HelpStackNavigator,
+} from "./StackNavigator";
 
 const Drawer = createDrawerNavigator();
 
 export const DrawerNavigator = () => {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
+  const homeTitle = t("Inicio");
+  const productsTitle = t("Productos");
+  const marketsTitle = t("Supermercados");
+  const helpTitle = t("Ayuda");
+
+  const iconsColor = "#00DE68";
+
   return (
     <Drawer.Navigator
       drawerContent={(props) => <Menu {...props} />}
       initialRouteName="Home"
-      screenOptions={{
-        headerLeft: () => (
-          <View style={styles.class1}>
-            <MenuIcon />
-          </View>
-        ),
-        headerRight: () => (
-          <View style={styles.class2}>
-            <TranslateIcon />
-            <HelpIcon />
-          </View>
-        ),
-      }}
     >
       <Drawer.Screen
         name="Home"
-        component={ListMainScreen}
+        component={InventoriesStackNavigator}
         options={{
-          title: t("Inicio"),
-          headerTitle: t("Inicio"),
+          title: homeTitle,
+          headerTitle: homeTitle,
           drawerIcon: () => (
-            <Ionicons name="home-outline" size={24} color="#00DE68" />
+            <Ionicons name="home-outline" size={24} color={iconsColor} />
           ),
         }}
       />
+      <Drawer.Screen
+        name="Products"
+        component={ProductsStackNavigator}
+        options={{
+          title: productsTitle,
+          headerTitle: productsTitle,
+          drawerIcon: () => (
+            <Feather name="help-circle" size={24} color={iconsColor} />
+          ),
+        }}
+      />
+      {/* <Drawer.Screen
+        name="Markets"
+        component={null}
+        options={{
+          title: marketsTitle,
+          headerTitle: marketsTitle,
+          drawerIcon: () => (
+            <Feather name="help-circle" size={24} color={iconsColor} />
+          ),
+        }}
+      /> */}
       <Drawer.Screen
         name="Ayuda"
-        component={HelpScreen}
+        component={HelpStackNavigator}
         options={{
-          title: t("Ayuda"),
+          title: helpTitle,
+          headerTitle: helpTitle,
           drawerIcon: () => (
-            <Feather name="help-circle" size={24} color="#00DE68" />
+            <Feather name="help-circle" size={24} color={iconsColor} />
           ),
         }}
       />
-      <Drawer.Screen
-        name="Productos"
-        component={SearchProductScreen}
-        options={{
-          title: t("Productos"),
-          drawerIcon: () => <Feather name="search" size={24} color="#00DE68" />,
-        }}
-      />
-      <Drawer.Screen
-        name="AddProduct"
-        component={AddProductScreen}
-        options={{
-          headerTitle: t("Agregar producto"),
-          title: t("Agregar producto"),
-          // drawerIcon: () => (
-          //   <Entypo name="add-to-list" size={24} color="#00DE68" />
-          // ),
-        }}
-      />
-      <Drawer.Screen
-        name="ProductInfo"
-        component={ProductInfoScreen}
-        options={{
-          headerTitle: t("Información de producto"),
-          title: t("Información de producto"),
-          // drawerIcon: () => (
-          //   <AntDesign name="infocirlceo" size={24} color="#00DE68" />
-          // ),
-        }}
-      />
-      <Drawer.Screen
-        name="NewList"
-        component={NewListScreen}
-        options={{ title: t("Agregar Lista"), headerTitle: t("Nueva Lista") }}
-      />
-      <Drawer.Screen
-        name="UpdateList"
-        component={UpdateListScreen}
-        options={{ title: t("Editar Lista"), headerTitle: t("Editar Lista") }}
-      />
-      <Drawer.Screen
-        name="ProductList"
-        component={ProductListScreen}
-        options={{ title: t("Lista de Productos"), headerTitle: t("Lista de Productos"), unmountOnBlur: true }}
-      />
-      
     </Drawer.Navigator>
   );
 };
-
-const styles = StyleSheet.create({
-  class1: {
-    marginLeft: 20,
-    marginRight: 20,
-    alignItems: "center",
-  },
-  class2: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginRight: 20,
-  },
-});
